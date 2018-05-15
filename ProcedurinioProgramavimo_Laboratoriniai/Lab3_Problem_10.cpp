96b8f018757b017b2731e202e181a57d106a876d
Julius Lungys <juliuslungys@gmail.com>
2018-05-16 01:46:39 +0300

    procedurinio prog 3 uzdavinys 10

--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_10.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_10.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 using namespace std;
-void sort2(vector<int> *vector);
+int calcSmallest(vector<int>* vector);
+int calcHighest(vector<int>* vector);
+double calcAverage(vector<int>* vector);
 int Lab3_Problem_10()
 {
 	ifstream in("seka.txt");
-	cout << in.rdstate() << endl;
-	if (!in || in.peek()==EOF) {
+	if (!in || in.peek() == EOF) {
 		cout << "Klaida nuskaitant duomenis";
 		return 0;
 	}
-	int n;
-	in >> n;
-	vector<int> vector(n);
-	for (int i = 0; i < n;i++){
-		in >> vector[i];
-		cout << vector[i] << " ";
-	}
-	in.close();
-	return 0;
-}
-void sort2(vector<int> &vectorp) {
-	int i, j, temporaryInt;
-	bool swapped;
-	for (i = 0; i < vectorp.size() - 1; i++) {
-		swapped = false;
-		for (j = 0; j < vectorp.size() - i - 1; j++) {
-			if (vectorp[j] > vectorp[j + 1]) {
-				temporaryInt = vectorp[j];
-				vectorp[j] = vectorp[j + 1];
-				vectorp[j + 1] = temporaryInt;
-				swapped = true;
-			}
+	else {
+		int n;
+		in >> n;
+		vector<int> vector(n);
+		for (int i = 0; i < n; i++) {
+			in >> vector[i];
 		}
-		if (swapped == false)
-			break;
+		in.close();
+
+		ofstream out("seka_rez.txt");
+		out << calcSmallest(&vector) << " " << calcHighest(&vector) << " ";
+		out << fixed << setprecision(2) << calcAverage(&vector);
+		out.close();
+		return 0;
 	}
+}
+int calcSmallest(vector<int>* vector) {
+	int smallest;
+	smallest = (*vector)[0];
+	for each (int i in (*vector)) {
+		if (i < smallest) {
+			smallest = i;
+		}
+	}
+	return smallest;
+}
+int calcHighest(vector<int>* vector) {
+	int highest;
+	highest = (*vector)[0];
+	for each (int i in (*vector)) {
+		if (highest < i) {
+			highest = i;
+		}
+	}
+	return highest;
+}
+double calcAverage(vector<int>* vector) {
+	double average = 0;
+	for each (int i in (*vector)) {
+		average += i;
+	}
+	average /= (*vector).size();
+	return average;
 }
