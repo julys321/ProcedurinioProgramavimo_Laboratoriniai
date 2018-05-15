904668ffe5c40c09add2d48c3dfff3062744b258
Julius Lungys <juliuslungys@gmail.com>
2018-05-16 02:36:39 +0300

    lab 3 uzd 11

--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_11.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_11.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include <fstream>
 using namespace std;
+int fibonaci(int n);
 int Lab3_Problem_11()
 {
-	cout << "Laboratorinis" << endl;
+	int n;
+	ifstream in("11.in");
+	in >> n;
+	in.close();
+	ofstream out("11.out");
+	out << fibonaci(n);
+	out.close();
 	return 0;
+}
+int fibonaci(int n) {
+	if (n == 0)
+		return 0;
+	else if (n == 1 || n == 2)
+		return 1;
+	else
+		return fibonaci(n - 1) + fibonaci(n - 2);
 }
