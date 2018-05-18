725c5cfbb4d3a3dd68b923a2833c9ee7b1bb9aaa
Julius Lungys <juliuslungys@gmail.com>
2018-05-18 21:49:42 +0300

    lab 3 14 ir 15

--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_14.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_14.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include <fstream>
 using namespace std;
+long long F(int n);
 int Lab3_Problem_14()
 {
-	cout << "Laboratorinis" << endl;
+	int n;
+	ifstream in("14.in");
+	in >> n;
+	in.close();
+	ofstream out("14.txt");
+	out << F(n);
+	out.close();
 	return 0;
+}
+long long F(int n) {
+	long long F[81];
+	F[0] = 0;
+	F[1] = 1;
+	for (int i = 2; i <= n; i++) F[i] = F[i - 1] + F[i - 2];
+	return F[n];
 }
--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_15.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_15.cpp
@@ -1,7 +1,42 @@
-#include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
+void rotateMatrix(vector<vector<int>> &matrix);
 int Lab3_Problem_15()
 {
-	cout << "Laboratorinis" << endl;
+
+	int n, k;
+	ifstream in("15.txt");
+	in >> n >> k;
+	vector<vector<int>> matrix(n, vector<int>(n));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			in >> matrix[i][j];
+		}
+	}
+	in.close();
+	while (k >= 4) {
+		k = k - 4;
+	}
+	for (int i = 0; i < k; i++) {
+		rotateMatrix(matrix);
+	}
+	ofstream out("15");
+	for (int i = 0; i < n; i++) {
+		out << endl;
+		for (int j = 0; j < n; j++) {
+			out << matrix[i][j] <<" ";
+		}
+	}
+	out.close();
 	return 0;
+}
+void rotateMatrix(vector<vector<int>> &matrix) {
+	vector<vector<int>> tempMatrix(matrix.size(), vector<int>(matrix.size()));
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix.size(); j++) {
+			tempMatrix[i][j] = matrix[matrix.size() - 1 - j][i];
+		}
+	}
+	matrix = tempMatrix;
 }
