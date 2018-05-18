239420c45ff1db95325ff41c25aa2a6d964a60e3
Julius Lungys <juliuslungys@gmail.com>
2018-05-18 20:32:27 +0300

    lab 3 prob 12

--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_12.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_12.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+void rotateRow(vector<char> &row, int i);
 int Lab3_Problem_12()
 {
-	cout << "Laboratorinis" << endl;
+	char temp;
+	vector<char> skc;
+	ifstream in("input.txt");
+	for (int i = 0; in >> temp; i++) {
+		skc.push_back(temp);
+	}
+	in.close();
+	reverse(skc.begin(),skc.end());
+	rotateRow(skc, skc.size());
+	ofstream out("o.txt");
+	for (char i:skc) {
+		out << i;
+	}
+	out.close();
 	return 0;
+}
+void rotateRow(vector<char> &row, int i) {
+	if (i > 1) {
+		i--;
+		row.insert(row.begin() + i, '\n');
+		rotateRow(row, i);
+	}
 }
