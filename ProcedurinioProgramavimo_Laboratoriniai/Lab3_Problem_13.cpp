cfc44505a120c0a4dbd6987b23b31b7be07ff586
Julius Lungys <juliuslungys@gmail.com>
2018-05-18 20:53:23 +0300

    lab 3 prob 13

--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_13.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_13.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <fstream>
 using namespace std;
+long long findDvigubasFactorialas(int n);
 int Lab3_Problem_13()
 {
-	cout << "Laboratorinis" << endl;
+	int n;
+	ifstream in("13.txt");
+	in >> n;
+	in.close();
+	ofstream out("13.out");
+	out << findDvigubasFactorialas(n);
+	out.close();
 	return 0;
+}
+long long findDvigubasFactorialas(int n) {
+	if (n <= 0)
+		return 1;
+	else
+		return  n * findDvigubasFactorialas(n - 2);
 }
