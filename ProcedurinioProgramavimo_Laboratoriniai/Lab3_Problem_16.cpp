63a78c34ac328b76841823e084bb8af1f956a3fe
Julius Lungys <juliuslungys@gmail.com>
2018-05-18 22:09:17 +0300

    lab 3 prob 16

--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_16.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_16.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int Lab3_Problem_16()
 {
-	cout << "Laboratorinis" << endl;
+	int a, n;
+	cin >> a >> n;
+	vector<int> memory;
+	memory.push_back(a);
+	for (int i = 1; i < n; i++) {
+		memory.push_back(memory[i - 1] * a);
+	}
+	cout << memory[n - 1];
 	return 0;
 }
