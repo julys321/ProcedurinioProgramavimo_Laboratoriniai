e3586b69a4b6ec6170ca7a25377f41d417880677
Julius Lungys <juliuslungys@gmail.com>
2018-05-19 00:54:30 +0300

    lab 3 prob 18 and 19

--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_18.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_18.cpp
@@ -1,7 +1,57 @@
+#include <fstream>
+#include <vector>
 #include <iostream>
 using namespace std;
+void removeLysve(vector<vector<char>> &matrix, int i, int j);
 int Lab3_Problem_18()
 {
-	cout << "Laboratorinis" << endl;
+
+	int n, m;
+	//ifstream in("18.txt");
+	//in >> n >> m;
+	cin >> n >> m;
+	vector<vector<char>> matrix(n, vector<char>(m));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			//in >> matrix[i][j];
+			cin >> matrix[i][j];
+		}
+	}
+	//in.close();
+	//work
+	int vysliuN = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (matrix[i][j] == '#') {
+				vysliuN++;
+				removeLysve(matrix, i, j);
+				//gyvas darbo sekimas
+				//for (int iout = 0; iout < n; iout++) {
+				//	cout << endl;
+				//	for (int jout = 0; jout < m; jout++) {
+				//		cout << matrix[iout][jout] << " ";
+				//	}
+				//}
+				//cout << endl;
+				//
+			}
+		}
+	}
+	cout << vysliuN;
 	return 0;
+}
+void removeLysve(vector<vector<char>> &matrix, int i, int j) {
+	matrix[i][j] = '.';
+	if ((i - 1) >= 0 && matrix[i - 1][j] == '#') {
+		removeLysve(matrix, i - 1, j);
+	}
+	if ((j + 1) < matrix[i].size() && matrix[i][j + 1] == '#') {
+		removeLysve(matrix, i, j + 1);
+	}
+	if ((i + 1) < matrix.size() && matrix[i + 1][j] == '#') {
+		removeLysve(matrix, i + 1, j);
+	}
+	if ((j - 1) >= 0 && matrix[i][j - 1] == '#') {
+		removeLysve(matrix, i, j - 1);
+	}
 }
--- ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_19.cpp
+++ ProcedurinioProgramavimo_Laboratoriniai/Lab3_Problem_19.cpp
@@ -1,7 +1,93 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+int compare(vector<short> &a, vector<short> &b);
+void subtract(vector<short> &a, vector<short> &b);
+void add(vector<short> &a, vector<short> &b);
 int Lab3_Problem_19()
 {
-	cout << "Laboratorinis" << endl;
+	vector<short> a;
+	vector<short> b;
+	char temp = '0';
+	vector<char> tempCharArray;
+	//ifstream in("19.txt");
+	cin.ignore(1);
+	while (true) {
+		//in.get(temp);
+		cin.get(temp);
+		if (temp == '\n' || temp == ' ')
+			break;
+		a.push_back(temp - '0');
+	}
+	temp = '0';
+	while (true) {
+		//in.get(temp);
+		cin.get(temp);
+		if (temp == '\n' || temp == ' ')
+			break;
+		b.push_back(temp - '0');
+	}
+	//in.close();
+	if (compare(a, b) == -1) {
+		subtract(a, b);
+	}
+	else {
+		add(a, b);
+	}
+	for (short i : a) {
+		cout << i;
+	}
+	
 	return 0;
+}
+int compare(vector<short> &a, vector<short> &b) {
+	if (a.size() > b.size())
+		return -1;
+	else if (a.size() < b.size())
+		return 1;
+	else {
+		for (int i = 0; i < a.size(); i++) {
+			if (a[i] > b[i])
+				return -1;
+			if (a[i] < b[i])
+				return 1;
+		}
+		return 0;
+	}
+}
+void subtract(vector<short> &a, vector<short> &b) {
+	vector<short> c;
+	short temp;
+	for (int i = 0; i < (b.size() - a.size()); i++) {
+		b.insert(b.begin(), 0);
+	}
+	for (int i = b.size()-1; i >= 0; i--) {
+		temp = a[i] - b[i];
+		if (temp < 0) {
+			a[i - 1]--;
+			a[i] += 10;
+		}
+		c.push_back(a[i] - b[i]);
+	}
+	reverse(c.begin(), c.end());
+	a = c;
+}
+void add(vector<short> &a, vector<short> &b) {
+	vector<short> c;
+	short temp = a.size() - b.size();
+	for (int i = 0; i < (a.size() - b.size()); i++) {
+		a.insert(a.begin(), 0);
+	}
+	for (int i = b.size() - 1; i >= 0; i--) {
+		temp = a[i] + b[i];
+		if (temp > 10) {
+			a[i - 1]++;
+			temp -= 10;
+		}
+		c.push_back(temp);
+	}
+	reverse(c.begin(), c.end());
+	a = c;
 }
